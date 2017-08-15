62ff4e40eef08474c5551f32c19a20723238a0dd
pat310 <patrick.trasborg@gmail.com>
2017-08-15 09:43:50 -0400

    fixing eslint errors in webpack.loaders

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -1,47 +1,47 @@
 module.exports = [
-	{
-		test: /\.jsx?$/,
-		exclude: /(node_modules|bower_components|public\/)/,
-		loader: "babel-loader",
-		query: {
-			presets: ['es2015', 'stage-2', 'react'],
-		}
-	},
-	{
-		test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "file-loader"
-	},
-	{
-		test: /\.(woff|woff2)$/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "url-loader?prefix=font/&limit=5000"
-	},
-	{
-		test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-	},
-	{
-		test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-	},
-	{
-		test: /\.gif/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "url-loader?limit=10000&mimetype=image/gif"
-	},
-	{
-		test: /\.jpg/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "url-loader?limit=10000&mimetype=image/jpg"
-	},
-	{
-		test: /\.png/,
-		exclude: /(node_modules|bower_components)/,
-		loader: "url-loader?limit=10000&mimetype=image/png"
-	},
+  {
+    test: /\.jsx?$/,
+    exclude: /(node_modules|bower_components|public\/)/,
+    loader: 'babel-loader',
+    query: {
+      presets: ['es2015', 'stage-2', 'react'],
+    },
+  },
+  {
+    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'file-loader',
+  },
+  {
+    test: /\.(woff|woff2)$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'url-loader?prefix=font/&limit=5000',
+  },
+  {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+  },
+  {
+    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+  },
+  {
+    test: /\.gif/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'url-loader?limit=10000&mimetype=image/gif',
+  },
+  {
+    test: /\.jpg/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'url-loader?limit=10000&mimetype=image/jpg',
+  },
+  {
+    test: /\.png/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'url-loader?limit=10000&mimetype=image/png',
+  },
   {
     test: /(\.jsx|\.js)$/,
     exclude: /node_modules/,
