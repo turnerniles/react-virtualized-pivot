bae9d49bfaa36261c25f93fbd0eb35f6732c333e
pat310 <patrick.trasborg@gmail.com>
2017-08-24 19:59:27 -0400

    increasing github fork banner zindex and decreasing banner size on mobile

--- src/app.jsx
+++ src/app.jsx
@@ -186,16 +186,18 @@ export default class App extends React.Component {
     return (
       <div>
         <a
-          href={'https://github.com/turnerniles/react-virtualized-pivot'}
-          target={'_blank'}
+          href="https://github.com/turnerniles/react-virtualized-pivot"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <img
+            className="github-fork"
             style={{
               'position': 'absolute',
               top: 0,
               right: 0,
               border: 0,
-              zIndex: 1,
+              zIndex: 5,
             }}
             // eslint-disable-next-line
             src={'https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'}
