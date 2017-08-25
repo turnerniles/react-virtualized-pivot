7cc3083b74d217dad94cbdd38e0fd88c54f8647c
pat310 <patrick.trasborg@gmail.com>
2017-08-24 20:07:05 -0400

    changing github fork me banner based on @media css prop

--- src/app.jsx
+++ src/app.jsx
@@ -192,16 +192,9 @@ export default class App extends React.Component {
         >
           <img
             className="github-fork"
-            style={{
-              'position': 'absolute',
-              top: 0,
-              right: 0,
-              border: 0,
-              zIndex: 5,
-            }}
             // eslint-disable-next-line
-            src={'https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'}
-            alt={'Fork me on GitHub'}
+            src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+            alt="Fork me on GitHub"
           />
         </a>
         <div
