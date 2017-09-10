c7fcf852ef3a52c7d63b69bcf26a2bdbe80ca5db
pat310 <patrick.trasborg@gmail.com>
2017-09-10 10:55:01 -0400

    moving row header on click to separate file

---
+++ src/components/Table/rowHeaderOnClick.js
@@ -0,0 +1,96 @@
+export default function onClick({
+  collapsedRows,
+  columnIndex,
+  data,
+  headerCounter,
+  onLeftGridCellClick,
+  originalArgs,
+  rawData,
+  rowIndex,
+  onToggleRow,
+}) {
+
+  function getCollapsedRows(rowNum, dataStr) {
+    const rows = rowNum in collapsedRows ? collapsedRows[rowNum].table : [];
+    const collapsedData = rowNum in collapsedRows ?
+      collapsedRows[rowNum][dataStr] :
+      [];
+
+    return collapsedData.reduce((acc, { type, value }, index) => {
+      const row = rows[index].row;
+
+      if (type === 'data') return acc.concat([value]);
+      return acc.concat(getCollapsedRows(row, dataStr));
+    }, []);
+  }
+
+  function getChildren(rowIndex, acc, startingDepth) {
+    const dataRow = data.slice(headerCounter)[rowIndex];
+    const rawDataRow = rawData.slice(headerCounter)[rowIndex];
+
+    if (!dataRow ||
+      (acc.children.length > 0 && startingDepth >= dataRow.depth)) {
+      return acc;
+    }
+
+    if (dataRow.type === 'data') {
+      const obj = {
+        children: acc.children.concat([dataRow.value]),
+        childrenData: acc.childrenData.concat([rawDataRow.value]),
+      };
+
+      return getChildren(rowIndex + 1, obj, startingDepth);
+    }
+
+    const obj = {
+      children: acc.children.concat(getCollapsedRows(dataRow.row, 'table')),
+      childrenData: acc.children
+        .concat(getCollapsedRows(dataRow.row, 'rawData')),
+    };
+
+    return getChildren(rowIndex + 1, obj, startingDepth);
+  }
+
+  function getRowHeaders(rowIndex) {
+    if (originalArgs.rows.length === 0) return {};
+
+    const slicedData = data.slice(headerCounter);
+    const { value, depth } = slicedData[rowIndex];
+    const acc = { [originalArgs.rows[depth]]: value[0] };
+    let nextDepth = depth - 1;
+    let counter = rowIndex - 1;
+
+    while (nextDepth >= 0) {
+      let nextValue = null;
+
+      while (nextValue === null) {
+        if (slicedData[counter].depth === nextDepth) {
+          nextValue = slicedData[counter].value[0];
+        }
+        counter--;
+      }
+
+      acc[originalArgs.rows[nextDepth]] = nextValue;
+      nextDepth--;
+    }
+
+    return acc;
+  }
+
+  if (columnIndex === 0) onToggleRow(rowIndex);
+
+  const { children, childrenData } = data.length > 0 ?
+    getChildren(rowIndex, {children: [], childrenData: []},
+      data[headerCounter + rowIndex].depth) :
+    [];
+
+  const rowHeaders = getRowHeaders(rowIndex);
+
+  onLeftGridCellClick({
+    children,
+    childrenData,
+    columnIndex,
+    rowHeaders,
+    rowIndex,
+  });
+}
