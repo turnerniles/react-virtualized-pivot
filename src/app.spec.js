ea9bae84755d05e6cadec6bc169c39ff15ce9f3d
Carol Scott <carol.a.scott@gmail.com>
2018-04-04 13:01:49 -0700

    Spec test setup complete

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,28 +1,27 @@
 import React from 'react';
+import Pivot from './components/Pivot/Pivot.jsx';
 import Select from 'react-select';
 import Papa from 'papaparse/papaparse.js';
-import ToggleDisplay from 'react-toggle-display';
-
 import App from './app.jsx';
 
-
+import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
 
 describe('App', () => {
-  it('should return true', () => {
-    expect(true).toEqual(true);
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+    });
+    it('should format seconds', () => {
+      const clock = shallow(<App/>);
+      let evt = {};
+      evt.target = {};
+      evt.target.value = 'true';
+      const actual = clock.instance().testFunc();
+
+      expect(actual).toBe(true);
   });
 });
-
-
-
-/*
-toggleUserData(evt) {
-  if (evt.target.value === 'true') {
-    this.setState({useSampleData: true, data: data.smallData});
-  } else {
-    this.setState({useSampleData: false, data: [[]]});
-  }
-}
-//*/
-
